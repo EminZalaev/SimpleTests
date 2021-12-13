dd108857c4c9bbc63d8c2d1fc1dd0791c01e6ba7
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2021-12-13 23:28:47 +0300

    add main_test.go func for test

---
+++ main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "tests/texting"
+
+type AddResults struct {
+  x int
+  y int
+  expected int
+}
+
+var addResults = []AddResults{
+  {1, 1, 2},
+}
+
+func TestAdd(t *testing.T){
+  for _, test := range addResults {
+    result := Add(test.x, test.y)
+    if result != test.expected {
+      t.Fatal("Expected Result Not Given")
+    }
+  }
+}
+
+func TestReadFile(t *testing.T){
+  data, err := iotil.ReadFile("testdata/test.data")
+  if err != nil{
+    t.Fatal("Could Not Open File")
+  }
+  if string(data) != "hello world"{
+    t.Fatal("String contents do not match expected")
+  }
+}

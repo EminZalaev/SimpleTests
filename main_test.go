437270d71fabcf0762f8756d054175942ed8c502
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2021-12-15 22:27:17 +0300

    work on mistakes

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "tests/texting"
+import (
+  "io/ioutil"
+	"testing"
+)
 
 type AddResults struct {
   x int
@@ -8,25 +11,25 @@ type AddResults struct {
   expected int
 }
 
-var addResults = []AddResults{
+var addResults = []AddResults {
   {1, 1, 2},
 }
 
-func TestAdd(t *testing.T){
+func TestAdd(t *testing.T) {
   for _, test := range addResults {
-    result := Add(test.x, test.y)
+    result := Add (test.x, test.y)
     if result != test.expected {
-      t.Fatal("Expected Result Not Given")
+      t.Fatal ("Expected Result Not Given")
     }
   }
 }
 
-func TestReadFile(t *testing.T){
-  data, err := iotil.ReadFile("testdata/test.data")
-  if err != nil{
-    t.Fatal("Could Not Open File")
+func TestReadFile(t *testing.T) {
+  data, err := ioutil.ReadFile ("testdata/test.data")
+  if err != nil {
+    t.Fatal ("Could Not Open File")
   }
-  if string(data) != "hello world"{
-    t.Fatal("String contents do not match expected")
+  if string(data) != "hello world" {
+    t.Fatal ("String contents do not match expected")
   }
 }
